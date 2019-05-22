55e825dafb24ced82d05db6acdfbce0ca0b8d601
Alexey Sumin <bardex@ya.ru>
2019-05-22 17:33:51 +0300

    search_filters

--- listeners/search_filter.go
+++ listeners/search_filter.go
@@ -33,6 +33,68 @@ func (b SearchBuilder) Equal(field string, value string) SearchFilter {
 	}
 }
 
+func (b SearchBuilder) NotEqual(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "<>",
+		Value:    value,
+	}
+}
+
+func (b SearchBuilder) Like(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "LIKE",
+		Value:    value,
+	}
+}
+
+func (b SearchBuilder) Exists(field string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "EXISTS",
+	}
+}
+
+func (b SearchBuilder) NotExists(field string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "NOT_EXISTS",
+	}
+}
+
+func (b SearchBuilder) Greater(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: ">",
+		Value:    value,
+	}
+}
+
+func (b SearchBuilder) GreaterOrEqual(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: ">=",
+		Value:    value,
+	}
+}
+
+func (b SearchBuilder) Less(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "<",
+		Value:    value,
+	}
+}
+
+func (b SearchBuilder) LessOrEqual(field string, value string) SearchFilter {
+	return SearchFilter{
+		Field:    field,
+		Operator: "<=",
+		Value:    value,
+	}
+}
+
 func (f SearchFilter) String() string {
 	s := ""
 	if f.ChildsUnion != "" {
@@ -46,7 +108,11 @@ func (f SearchFilter) String() string {
 			s = s + ")"
 		}
 	} else {
-		s = f.Field + f.Operator + f.Value
+		if f.Value != "" {
+			s = f.Field + " " + f.Operator + " " + f.Value
+		} else {
+			s = f.Operator + " " + f.Field
+		}
 	}
 	return s
 }
--- listeners/search_filter_test.go
+++ listeners/search_filter_test.go
@@ -7,7 +7,95 @@ import (
 func TestSearchBuilderEqual(t *testing.T) {
 	b := SearchBuilder{}
 	f := b.Equal("user", "Alice")
-	expect := "user=Alice"
+	expect := "user = Alice"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderNotEqual(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.NotEqual("user", "Alice")
+	expect := "user <> Alice"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderLike(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.Like("user", "Alice")
+	expect := "user LIKE Alice"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderExists(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.Exists("user")
+	expect := "EXISTS user"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderNotExists(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.NotExists("user")
+	expect := "NOT_EXISTS user"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderGreater(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.Greater("level", "0")
+	expect := "level > 0"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderGreaterOrEqual(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.GreaterOrEqual("level", "0")
+	expect := "level >= 0"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderLess(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.Less("level", "0")
+	expect := "level < 0"
+	actual := f.String()
+
+	if actual != expect {
+		t.Fatalf("Expected: '%s' but actual: '%s'", expect, actual)
+	}
+}
+
+func TestSearchBuilderLessOrEqual(t *testing.T) {
+	b := SearchBuilder{}
+	f := b.LessOrEqual("level", "0")
+	expect := "level <= 0"
 	actual := f.String()
 
 	if actual != expect {
@@ -22,11 +110,14 @@ func TestSearchBuilderNesting(t *testing.T) {
 		b.Equal("status", "0"),
 		b.Or(
 			b.Equal("user", "Alice"),
-			b.Equal("user", "Bob"),
+			b.And(
+				b.Equal("user", "Bob"),
+				b.Greater("age", "18"),
+			),
 		),
 	)
 
-	expect := "(status=0 AND (user=Alice OR user=Bob))"
+	expect := "(status = 0 AND (user = Alice OR (user = Bob AND age > 18)))"
 	actual := f.String()
 
 	if actual != expect {
